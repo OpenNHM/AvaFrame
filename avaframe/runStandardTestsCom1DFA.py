de57a5d3179e2cc5085e09e3cebf359be1139168
Felix Oesterle <6945681+fso42@users.noreply.github.com>
2025-11-19 14:44:37 +0100

    chore(benchmarks): update avaInclinedPlaneEntresTest; fixes #1208
      - update gitignore

--- avaframe/runStandardTestsCom1DFA.py
+++ avaframe/runStandardTestsCom1DFA.py
@@ -254,10 +254,10 @@ testDictList = tU.readAllBenchmarkDesDicts(info=False)
 # filter benchmarks for tag standardTest
 # filterType = "TAGS"
 # valuesList = ["resistance"]
-filterType = "TAGS"
-valuesList = ["standardTest", "standardTestSnowGlide"]
-# filterType = "NAME"
-# valuesList = ["avaGarNullTest6", "avaGarNullTest2"]
+# filterType = "TAGS"
+# valuesList = ["standardTest", "standardTestSnowGlide"]
+filterType = "NAME"
+valuesList = ["avaInclinedPlaneEntresTest"]
 
 testList = tU.filterBenchmarks(testDictList, filterType, valuesList, condition="or")
 
